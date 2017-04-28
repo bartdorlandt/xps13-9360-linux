f77d08fdf7404c44c311dd2706ee7f204052668f
bart@bart-netyce <bart@bamweb.nl>
2017-04-28 09:57:39 +0200

    initial files

---
+++ disable-ctrl-alt-del.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl mask ctrl-alt-del.target
+sudo systemctl daemon-reload
---
+++ enable-super-key.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Delete the dell-super-key package"
+sudo apt-get remove dell-super-key –y
+
+echo "Open the browser and follow the other instructions:"
+echo "http://www.dell.com/support/article/us/en/04/HOW12108/how-to-enable-the-ubuntu-super-key-on-dell-oem-ubuntu-installations?lang=EN"
+
